72a2938fed4f18f4bdd809b4122dbcc2310745fd
Aidyn <112602044+PowerForLuxx@users.noreply.github.com>
2022-11-05 15:39:26 +0600

    init of first file

--- TestClass.java
+++ TestClass.java
@@ -2,5 +2,6 @@ public class TestClass {
     public static void main(String[] args){
         System.out.println("Hi!");
         System.out.println("Soooooool");
+        System.out.println("Jumboooo");
     }
 }
